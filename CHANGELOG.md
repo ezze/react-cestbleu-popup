c5cb2ccb6496d47783d9445f98fe0ae72ac36bda
Dmitriy Pushkov <ezze@ezze.org>
2017-12-07 18:29:14 +0300

    Module is renamed.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 ## Changelog
 
+### 0.3.3 (2017-02-07)
+
+- Module name is changed from `reactCestbleuPopup` to `ReactCestbleuPopup`.
+
 ### 0.3.2 (2017-02-02)
 
 - `classnames` and `prop-types` are removed from `globals`.
--- rollup.config.es.js
+++ rollup.config.es.js
@@ -1,7 +1,7 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-    name: 'reactCestbleuPopup',
+    name: 'ReactCestbleuPopup',
     input: 'src/index.js',
     output: {
         file: 'dist/react-cestbleu-popup.es.js',
